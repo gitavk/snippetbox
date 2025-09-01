f86671d39119e4a7dc18f3efbd2e88d6439f127d
gitavk <3apa3ka3@gmail.com>
2025-09-01 17:45:23 +0200

    Web basic.

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
-import "fmt"
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+// Add a snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
 func main() {
-  fmt.Println("Hello world!")
+	// Register the two new handler functions and corresponding route patterns with
+	// the servemux, in exactly the same way that we did before.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	log.Print("starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
