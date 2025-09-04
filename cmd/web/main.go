7946393f93cb8f0cfc3e6e72a751fecacffb5bbf
gitavk <3apa3ka3@gmail.com>
2025-09-04 11:38:21 +0200

    Database connection.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type application struct {
@@ -13,16 +16,46 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "postgres://postgres:password@localhost:5432/snippetbox", "PostgreSQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	var db_time string
+	err = db.QueryRow("select NOW() ").Scan(&db_time)
+	if err != nil {
+		logger.Error("QueryRow failed", "err", err)
+		os.Exit(1)
+	}
+
+	logger.Info("Database info:", "db_time", db_time)
+
 	app := &application{
 		logger: logger,
 	}
 
 	logger.Info("starting server on", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routers())
+	err = http.ListenAndServe(*addr, app.routers())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
 }
