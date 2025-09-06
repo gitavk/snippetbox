1a0612097d52e631112086603be9bbc44f0eb940
gitavk <3apa3ka3@gmail.com>
2025-09-06 16:09:12 +0200

    Displaying dynamic data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-	// "html/template"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -57,7 +57,28 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%+v \n", snippet)
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+  
+  data := templateData{
+    Snippet: snippet,
+  }
+
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
---
+++ cmd/web/templates.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/gitavk/snippetbox/internal/models"
+
+// Define a templateData type to act as the holding structure for
+type templateData struct {
+  Snippet models.Snippet
+}
