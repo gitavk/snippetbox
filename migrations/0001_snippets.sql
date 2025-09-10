4f4a67d0f179265f9ab9931ba72a929f95ff7fca
gitavk <3apa3ka3@gmail.com>
2025-09-10 20:03:03 +0200

    Migrations sql.

---
+++ migrations/0001_snippets.sql
@@ -0,0 +1,10 @@
+CREATE TABLE snippets (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    created TIMESTAMP NOT NULL,
+    expires TIMESTAMP NOT NULL
+);
+
+-- Add an index on the created column
+CREATE INDEX idx_snippets_created ON snippets(created);
