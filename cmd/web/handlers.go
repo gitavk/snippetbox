62a586cee86b7900d443210af5053c8b4547b539
gitavk <3apa3ka3@gmail.com>
2025-09-11 12:12:50 +0200

    Validate user input.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -99,7 +99,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 
 type userSignupForm struct {
 	Name     string `form:"name"`
-	Email    string `form:"enail"`
+	Email    string `form:"email"`
 	Password string `form:"password"`
 	validator.Validator
 }
@@ -109,9 +109,34 @@ func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
 	data.Form = userSignupForm{}
 	app.render(w, r, http.StatusOK, "signup.tmpl", data)
 }
+
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	var form userSignupForm
+
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	// Validate the form contents using our helper functions.
+	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
+
+	// If there are any errors, redisplay the signup form along with a 422
+	// status code.
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "signup.tmpl", data)
+		return
+	}
+	// Otherwise send the placeholder response (for now!).
 	fmt.Fprintln(w, "Create a new user...")
 }
+
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Display a form for logging in a user...")
 }
