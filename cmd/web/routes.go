27325bc20795923724b0a6fc2a14493bc2b2f740
gitavk <3apa3ka3@gmail.com>
2025-09-13 11:34:17 +0200

    application required authentification method.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,14 +13,17 @@ func (app *application) routers() http.Handler {
 
 	mux.Handle("GET /{$}", session(http.HandlerFunc(app.home)))
 	mux.Handle("GET /snippet/view/{id}", session(http.HandlerFunc(app.snippetView)))
-	mux.Handle("GET /snippet/create", session(http.HandlerFunc(app.snippetCreate)))
-	mux.Handle("POST /snippet/create", session(http.HandlerFunc(app.snippetCreatePost)))
 
 	mux.Handle("GET /user/signup", session(http.HandlerFunc(app.userSignup)))
 	mux.Handle("POST /user/signup", session(http.HandlerFunc(app.userSignupPost)))
 	mux.Handle("GET /user/login", session(http.HandlerFunc(app.userLogin)))
 	mux.Handle("POST /user/login", session(http.HandlerFunc(app.userLoginPost)))
-	mux.Handle("POST /user/logout", session(http.HandlerFunc(app.userLogoutPost)))
+
+	is_auth := app.requireAuthentication
+
+	mux.Handle("GET /snippet/create", session(is_auth(http.HandlerFunc(app.snippetCreate))))
+	mux.Handle("POST /snippet/create", session(is_auth(http.HandlerFunc(app.snippetCreatePost))))
+	mux.Handle("POST /user/logout", session(is_auth(http.HandlerFunc(app.userLogoutPost))))
 
 	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
 }
