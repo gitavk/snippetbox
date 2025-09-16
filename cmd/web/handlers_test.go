6f1e6563e559d51e321c43cb7502e02d1265c1b2
gitavk <3apa3ka3@gmail.com>
2025-09-16 21:41:10 +0200

    Testing Html form.

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -5,10 +5,134 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/gitavk/snippetbox/internal/assert"
 )
+
+func TestUserSignup(t *testing.T) {
+	// Create the application struct containing our mocked dependencies and
+	// establish a new test server.
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routers())
+	defer ts.Close()
+	const (
+		validName     = "Bob"
+		validPassword = "validPa$$word"
+		validEmail    = "bob@example.com"
+		formTag       = "<form action='/user/signup' method='POST' novalidate>"
+	)
+
+	tests := []struct {
+		name              string
+		userName          string
+		userEmail         string
+		userPassword      string
+		useValidCSRFToken bool
+		wantStatus        int
+		wantFormTag       string
+	}{
+		{
+			name:              "Valid submission",
+			userName:          validName,
+			userEmail:         validEmail,
+			userPassword:      validPassword,
+			useValidCSRFToken: true,
+			wantStatus:        http.StatusSeeOther,
+		},
+		{
+			name:              "Invalid CSRF Token",
+			userName:          validName,
+			userEmail:         validEmail,
+			userPassword:      validPassword,
+			useValidCSRFToken: false,
+			wantStatus:        http.StatusBadRequest,
+		},
+		{
+			name:              "Empty name",
+			userName:          "",
+			userEmail:         validEmail,
+			userPassword:      validPassword,
+			useValidCSRFToken: true,
+			wantStatus:        http.StatusUnprocessableEntity,
+			wantFormTag:       formTag,
+		},
+		{
+			name:              "Empty email",
+			userName:          validName,
+			userEmail:         "",
+			userPassword:      validPassword,
+			useValidCSRFToken: true,
+			wantStatus:        http.StatusUnprocessableEntity,
+			wantFormTag:       formTag,
+		},
+		{
+			name:              "Empty password",
+			userName:          validName,
+			userEmail:         validEmail,
+			userPassword:      "",
+			useValidCSRFToken: true,
+			wantStatus:        http.StatusUnprocessableEntity,
+			wantFormTag:       formTag,
+		},
+		{
+			name:              "Invalid email",
+			userName:          validName,
+			userEmail:         "bob@example.",
+			userPassword:      validPassword,
+			useValidCSRFToken: true,
+			wantStatus:        http.StatusUnprocessableEntity,
+			wantFormTag:       formTag,
+		},
+		{
+			name:              "Short password",
+			userName:          validName,
+			userEmail:         validEmail,
+			userPassword:      "pa$$",
+			useValidCSRFToken: true,
+			wantStatus:        http.StatusUnprocessableEntity,
+			wantFormTag:       formTag,
+		},
+		{
+			name:              "Duplicate email",
+			userName:          validName,
+			userEmail:         "dupe@example.com",
+			userPassword:      validPassword,
+			useValidCSRFToken: true,
+			wantStatus:        http.StatusUnprocessableEntity,
+			wantFormTag:       formTag,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Reset the cookie jar for each sub-test.
+			ts.resetClientCookieJar(t)
+			// Make a GET /user/signup signup request. This will automatically
+			// add the CSRF cookie from the response to the test client's cookie
+			// jar, and we can extract the CSRF token from the response body.
+			res := ts.get(t, "/user/signup")
+			// Build up the form values for the sub-test, including the CSRF
+			// token if appropriate.
+			form := url.Values{}
+			form.Add("name", tt.userName)
+			form.Add("email", tt.userEmail)
+			form.Add("password", tt.userPassword)
+			if tt.useValidCSRFToken {
+				form.Add("csrf_token", extractCSRFToken(t, res.body))
+			}
+			// Make the POST /user/signup request using the form values we
+			// created above. The request will automatically include the CSRF
+			// cookie from the test client's cookie jar.
+			res = ts.postForm(t, "/user/signup", form)
+			// And finally, test the response data.
+			assert.Equal(t, res.status, tt.wantStatus)
+			assert.StringContains(t, res.body, tt.wantFormTag)
+		})
+	}
+
+}
 
 func TestPingRoute(t *testing.T) {
 	app := newTestApplication(t)
